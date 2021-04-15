c77af8055aab7dff4bd220837a83f39292e40743
Patrick Drechsler <patrick@pdrechsler.de>
2021-04-15 21:45:22 +0200

    :memo: comment difference between add/create and edit/update

--- WineTasting/WineTastingWeb/WineController.fs
+++ WineTasting/WineTastingWeb/WineController.fs
@@ -4,9 +4,9 @@ open Saturn
 
 let wineController = controller {
     index (fun ctx -> "Index handler version 1" |> Controller.text ctx) //View list of users
-    add (fun ctx -> "Add handler version 1" |> Controller.text ctx) //Add a user
-    create (fun ctx -> "Create handler version 1" |> Controller.json ctx) //Create a user
+    //add (fun ctx -> "Add handler version 1" |> Controller.text ctx) //Add a user // "Show form for create"
+    create (fun ctx -> "Create handler version 1" |> Controller.json ctx) //Create a user // this is the actual POST command
     show (fun ctx id -> (sprintf "Show handler version 1 - %i" id) |> Controller.text ctx) //Show details of a user
-    edit (fun ctx id -> (sprintf "Edit handler version 1 - %i" id) |> Controller.text ctx)  //Edit a user
-    update (fun ctx id -> (sprintf "Update handler version 1 - %i" id) |> Controller.text ctx)  //Update a user
+    //edit (fun ctx id -> (sprintf "Edit handler version 1 - %i" id) |> Controller.text ctx)  //Edit a user // "Show form for update"
+    update (fun ctx id -> (sprintf "Update handler version 1 - %i" id) |> Controller.text ctx)  //Update a user// this is the actual PUT command
 }
